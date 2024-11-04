086b6fb161f86369632f518af3796cc6d527736a
Ahisa Puebla Souza <ahisapuebla@gmail.com>
2024-11-04 22:12:47 +0100

    second commit

--- RevitAddinBootcamp/cmdSkills01.cs
+++ RevitAddinBootcamp/cmdSkills01.cs
@@ -14,7 +14,7 @@
             // Delete the TaskDialog below and add your code
             TaskDialog.Show("Module 01 Skills", "Got Here to Skills 01!");
             TaskDialog.Show("test", "Testing to see if this makes it to Github");
-            TaskDialog.Show("test", "Testing to see if this return Result.Succeded;
+            TaskDialog.Show("test", "Testing to see if this makes it back to VisualStudio");
 
             return Result.Succeeded;
         }
